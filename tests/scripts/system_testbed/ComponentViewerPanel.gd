42b1d6797e8d59d37849e114653c115e54523265
Invisible Acropolis <invisibleacropolis@gmail.com>
2025-09-19 04:26:40 -0400

    Add System Testbed layout and panel scaffolds

---
+++ tests/scripts/system_testbed/ComponentViewerPanel.gd
@@ -0,0 +1,10 @@
+extends PanelContainer
+class_name ComponentViewerPanel
+"""
+Renders component manifests for the entity currently selected in the inspector.
+The full editor will stream component metadata into rich editing widgets in future updates.
+"""
+
+func _ready() -> void:
+    """Initialises the component viewer once backing services exist."""
+    pass
