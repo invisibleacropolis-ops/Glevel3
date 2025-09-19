55da06ad7018c3c9de37bfcfca4652d9a1ba5742
invisibleacropolis-ops <invisibleacropolis@gmail.com>
2025-09-19 02:31:45 -0400

    31

--- src/core/Component.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name Component
-
-## Base resource for game components in the entity-component system.
-## Components hold data and helper logic but no scene tree behaviour.
-## Designed for Godot 4.4.1.
-
-# This class intentionally contains no functionality; it exists to provide
-# a common ancestor for all component resources.
