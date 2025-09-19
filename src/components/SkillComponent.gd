909a7f9d7b651734c3c3e8345311d22d2a0d93a1
invisibleacropolis-ops <invisibleacropolis@gmail.com>
2025-09-19 03:24:38 -0400

    546654

--- src/components/SkillComponent.gd
+++
@@ -1,6 +0,0 @@
-extends "res://src/core/Component.gd"
-class_name SkillComponent
-
-## Component that holds an entity's available skills.
-
-@export var skills: Array[Resource] = []
--- src/core/skills/BaseMeleeSkill.gd
+++
@@ -1,9 +0,0 @@
-extends "res://src/core/Skill.gd"
-class_name BaseMeleeSkill
-
-## A specialized Skill resource for the Melee category.
-## Inherits all properties from Skill.gd.
-
-func _init():
-    # Automatically set the category for any new resource of this type.
-    category = ULTEnums.SkillCategory.MELEE
